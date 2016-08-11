64367debf36b7596b91c1a48ea7a28d49d82def9
Peter Enemark Lund <cosius@gmail.com>
2016-08-11 09:47:51 +0200

    getAdmLimits 2
    New Version of getAdmLimits and more advanced code to test on.

---
+++ R/getAdmLimits2.R
@@ -0,0 +1,55 @@
+### getAdmLimits2.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds 
+## created: Aug  4 2016 (19:43) 
+## Version: 
+## last-updated: Aug  4 2016 (22:07) 
+##           By: Thomas Alexander Gerds
+##     Update #: 12
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+##' R-version of the sas code from hell.
+##'
+##' For now pattype is ignored. 
+##' @title Thomas's first R-version of the sas code from hell 
+##' @param dt data.table which provides variables: pnr, recno, inddto, uddto, pattype
+##' @return nothing. but input data.table is modified such that all intermediate admission records are removed. This means that overlapping admission
+##' intervals in the input data are combined into one admission.
+##' @examples
+##' data(samplepop)
+##' ## take a copy to preserve the object samplepop 
+##' pop=copy(samplepop)
+##' setDT(pop)
+##' newpop=getAdmLimits(pop)
+##' print(print(newpop))
+##' @export 
+##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
+
+getAdmLimits2 <- function(dt2){
+    stopifnot(is.data.table(dt2))
+    wdt2=copy(dt2)
+    ## sort by pnr and admission dates 
+    ## sort by pnr and admission dates 
+    setkey(wdt2,pnr,inddto)
+    ## latest admission date by pnr
+    wdt2[,max.uddto:=cummax(uddto),by=pnr]
+    ##start of each admission
+    wdt2[,startadm:=1L*(inddto>shift(max.uddto)),by=pnr]
+    wdt2[.(unique(pnr)),startadm:=1L,mult="first"]
+    ## overlapping admissions
+    wdt2[,startadm:=cumsum(startadm),by=pnr]
+    ##start and end date for the hospitalization the admission is part of.
+    wdt2[,max.uddto:=max(max.uddto),by=c("pnr","startadm")]
+    wdt2[,startadm :=min(inddto)   ,by=c("pnr","startadm")] 
+    ##Change names to what the two variables now contain
+    setnames(wdt2,c("startadm","max.uddto"),c("first.inddto","last.uddto"))
+}
+
+#----------------------------------------------------------------------
+### getAdmLimits2.R ends here
