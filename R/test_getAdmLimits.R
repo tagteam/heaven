8de9b430dd947f41194f7278b81c063372574a25
Jannik Langtved Pallisgaard <jannikjannik@gmail.com>
2016-08-11 11:03:42 +0200

    samplegenerator and test getAdmLimits

--- R/test_getAdmLimits.R
+++ R/test_getAdmLimits.R
@@ -4,11 +4,19 @@ diag<-getAdmLimits(diag)
 diag<-diag[order(pnr,inddto),]
 View(diag)
 
-##Problems solved ##
-#regbnum 34 are duplicates 
-#regnum 29 is a hospital stay within other hospital stays
 
-##Problem not solved ##
+##Problems not solved ##
+# does not remove duplicates
 #regnum 35 is a hospital stay for 20000 days
 #regnum 37 is a hospital stay with a negative duration
 
+library(data.table)
+diag<-data.table(sample.diag)
+diag<-getAdmLimits2(diag)
+diag<-diag[order(pnr,inddto),]
+View(diag)
+
+##Problems not solved ##
+#does not remove duplicates
+#regnum 35 is a hospital stay for 20000 days
+#regnum 37 is a hospital stay with a negative duration
