72eb0aecbb9bedc7a56e95ecd5f7f27d07b9d13e
helenecharlotte <helenerytgaard@gmail.com>
2016-09-19 12:02:03 +0200

    test connection git

---
+++ R/test-hcr.R
@@ -0,0 +1,26 @@
+### test-hcr.R --- 
+#----------------------------------------------------------------------
+## author: Helene Charlotte Rytgaard
+## created: September 19 2016
+## Version: 
+## last-updated: September 19 2016
+##           By: Helene Charlotte Rytgaard
+##     Update #: 1
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+#code from hell----
+##' R-version of the sas code from hell.
+##'
+##' Ignores pattype 
+##' @title Test 
+##' @param 
+##' @return 
+##' @examples
+##' @export 
+##' @author Helene Charlotte Rytgaard
