8fb748e8b693c6b6bcf29620aead859e3669c3c4
helenecharlotte <helenerytgaard@gmail.com>
2016-11-18 10:43:34 +0100

    updating print function

--- R/dpp.R
+++ R/dpp.R
@@ -8,7 +8,7 @@ dpp <- function() {
   out = structure(list(drugdb = NULL,
                        admdb  = NULL, 
                        drugs  = NULL, 
-                       period = c(as.Date("1994-01-01"), as.Date(Sys.Date()+5000)),
+                       period = c(1, 1e10),
                        N      = 2),
                   class = "dpp")
   out
@@ -28,13 +28,17 @@ print.dpp <- function(dpp) {
     doses = lapply(dpp$drugs, function(x) x$doses)
     cat("with corresponding dose values:", "\n")
     print(do.call("rbind", lapply(doses, function(x) as.data.frame(x))))
+    cat("\n")
+    if (length(dpp$drugdb) > 0) {
+      dosesindata = unique(dpp$drugdb$strength[dpp$drugdb$atc %in% unlist(drugs)])
+      cat("Doses found in data:", paste(dosesindata, collapse = ", "), "\n")
+    }
+    cat("\n")
   } 
-  cat("\n")
   if (length(dpp$N) > 0)
     cat("Using N =", dpp$N, "prescriptions back in time", "\n")
-  if (length(dpp$period) > 1)
+  if (length(dpp$period) > 1 & dpp$period[1] != 1)
     cat("Only interested in prescriptions between", as.character(dpp$period[1]), "and", as.character(dpp$period[2]))  
+
   cat("\n")
 } 
-
-
