f5d91b17a77e5f9eee5ec3c5d6d3dbd213a8ff2a
helenecharlotte <helenerytgaard@gmail.com>
2016-11-28 10:30:17 +0100

    plot changes

--- worg/code-for-interface-illustration.R
+++ worg/code-for-interface-illustration.R
@@ -2,54 +2,24 @@
 
 library(heaven)
 library(Publish)
-
-d <- dpp()
-
 set.seed(8)
-drugdata <- simPrescriptionData(10, startDate = "2006-01-01")
+drugdata <- simPrescriptionData(10)
+admdata <- simAdmissionData(10)
+d <- dpp()
 drugdb(d) <- drugdata
-org(head(d$drugdb))
-
-admdata <- rbind(simAdmissionData(10, startDate = "2006-01-01"))
 admdb(d) <- admdata
-org(head(d$admdb))
-
-drug(d, ex1) <- atc("A12B")
-drug(d, ex2) <- atc("A07")
-drug(d, ex1) <- atc(c("A12B"))
-drug(d, ex2) <- atc(c("A07"))
-drug(d, ex1) <- pack(c(750, 75), 
-                     min = c(250, 25),
-                     max = c(1000, 100), 
-                     def = c(750, 100))
-drug(d, ex2) <- pack(c(200, 400, 500), 
+drug(d, "drug1") <- atc("A12B")
+drug(d, "drug1") <- pack(c(750, 75), 
+                            min = c(250, 25), 
+                            max = c(1000, 100), 
+                            def = c(750, 100))
+drug(d, "drug2") <- atc("A07")
+drug(d, "drug2") <- pack(c(200, 400, 500), 
                      min = c(100, 100, 250),
                      max = c(400, 500, 1000), 
                      def = c(300, 200, 500))
+pwindow(d) <- 3
+out <- process(d, keep_data = TRUE)
 
-plot(d, id = 4, drug = "ex1")
-N(d) <- 3
-maxrep(d) <- 50
+plot(out, id = 5, trace = TRUE)
 
-ex <- process(d, out=FALSE, id = 3) 
-
-(ex1out <- ex$ex1)
-(ex2out <- ex$ex2)
-org(head(ex1out[, names(ex1out) %in% c("id", "X", "B", "E", "R")]))
-
-plot(ex, id=3, drug="ex2")
-
-ex1 <- process(d, id=2, out=FALSE) 
-ex1
-
-
-
-
-is(ex1, "dppout")
-ex2 <- process(d, id=2, out=TRUE) 
-ex2
-attr(ex2, "class")
-plot(ex2)
-plot(ex1)
-attr(ex1, "type")
-inherits(ex2, "class")
