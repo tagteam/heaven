16ac6342ab710b4b8334637ed4eaed7c5fd1a826
cosius <cosius@gmail.com>
2016-12-08 15:06:32 +0100

    more

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -39,3 +39,14 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// timesTwo
+NumericVector timesTwo(NumericVector x);
+RcppExport SEXP heaven_timesTwo(SEXP xSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericVector >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(timesTwo(x));
+    return rcpp_result_gen;
+END_RCPP
+}
