13311f0f4a5d7510742926a5240093efc3237273
Thomas Alexander Gerds <tag@biostat.ku.dk>
2017-08-09 09:52:21 +0200

    fixed parallel code

--- R/RiskSetMatch.R
+++ R/RiskSetMatch.R
@@ -87,145 +87,148 @@
 #'  ,reuseCases=TRUE,reuseControls=TRUE,cores=2)
 RiskSetMatch <- function(ptid,event,terms,dat,Ncontrols,reuseCases=FALSE,reuseControls=FALSE,caseIndex=NULL,
                          controlIndex=NULL, NoIndex=FALSE, cores=1){
-#browser()  
-  #ptid - pnr i DST id
-  #event 0/1 distinguishes cases from controls
-  #terms c(1,2,3) - list of vairables to match by
-  #dat - dataset with all variables
-  #Ncontrols - number of controls to provide
-  #reuseCases - T og F or NULL - can a case be a control prior to being a case?
-  #reuseControls - T or F or NULL
-  #caseIndex - Integer or date, date where controls must be prior
-  #controlIndex - Index date for controls
-  options(warn=-1)
-  # copy input data
-  datt <- copy(dat)
-  if (NoIndex) noindex <- 1L else noindex <- 0L # allows omitting index vectors
-  # Check data.table
-  if (!is.data.table(dat)) stop("data not data.table")
-  # combine matching variables to single term - cterms
-  datt[, cterms :=do.call(paste0,.SD),.SDcols=terms] 
-  # Select relevant part of table for matching
-  if (!NoIndex){
-    alldata <- datt[,.SD,.SDcols=c(ptid,caseIndex,controlIndex,event,"cterms") ]
-    setnames(alldata,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
-  }
-  else{
-    alldata <- datt[,.SD,.SDcols=c(ptid,event,"cterms") ]
-    setnames(alldata,c(".ptid",".event",".cterms"))
-  }
-  #Checking variables (partially)
-  numbers <- table(alldata[,.event])
-  if  ((names(numbers)==c("0","1")) !=c(T,T)) stop (" Event not 0 or 1 ")
-  # prepare to split and split
-  setkey(alldata,.cterms)
-  split.alldata <- split(alldata,by=".cterms")
-  if (cores<2){
-    # Prepare progress bar
-    totalprogress <-as.numeric(length(split.alldata)/1000)
-    pb <-txtProgressBar(min = 0, max = 1000, initial = 0, char = "=",
-                        width = NA, title, label, style = 1, file = "")
-    progress <- 0;
-    # Select controls - rbind of each split-member that selects controls
-    selected.controls <- do.call(rbind,lapply(split.alldata,function(controls){
-#browser()    
-      # Setnames because data.table called from function
-      if (!NoIndex) setnames(controls,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
-      else setnames(controls,c(".ptid",".event",".cterms"))
-      setkey(controls,.event,.ptid)
-      # Define cases in particular match-group
-      cases <- controls[.event==1]
-      setkey(cases,.ptid)
-      # Uf cases cannot becom controls they are removed from controls
-      if (!reuseCases) controls <- controls[.event==0]
-      #find lengths of controls and cases
-      Tcontrols<-dim(controls)[1]
-      Ncases<-dim(cases)[1]
-      # sort controls by random number - so that they can be selected sequentially
-      set.seed(17)
-      controls[,random:=runif(.N,1,Ncontrols*10)]
-      setkey(controls,random)
-      # vectors for Rcpp
-      CONTROLS <- as.character(controls[,.ptid])
-      CASES <- as.character(cases[,.ptid])
-      NreuseControls<-as.numeric(reuseControls) # Integerlogic for Rcpp
-      if(!NoIndex){
-        controlIndex <- controls[,.controlIndex]
-        caseIndex <- cases[,.caseIndex]
-      } else {
-        controlIndex <- 0L
-        caseIndex <- 0L
-      }
-      Output <- heaven::Matcher(Ncontrols, Tcontrols, Ncases, NreuseControls,  
-                controlIndex, caseIndex, CONTROLS, CASES,noindex)
-      setDT(Output)
-      progress <<- progress+1/totalprogress
-      #Progress bar
-        setTxtProgressBar(pb,progress)
-        flush(stdout())
-      Output
-    })) # end function and do.call
-  } #end if cores<2
-  else
-  {
-    CLUST <- parallel::makeCluster(min(parallel::detectCores(),cores))
-    print(CLUST)
-    parallel::clusterExport(CLUST, c("Matcher"))#,"NreuseControls","noindex","reuseCases","NoIndex"),envir=environment())
-    parallel::clusterEvalQ(CLUST, library(data.table))
-    parallel::clusterEvalQ(CLUST, library(heaven))
-    # Select controls - rbind of each split-member that selects controls
-    selected.controls <- do.call(rbind,parallel::parLapply(CLUST,split.alldata,function(controls){
-      # Setnames because data.table called from functio
-      if (!NoIndex) setnames(controls,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
-      else setnames(controls,c(".ptid",".event",".cterms"))
-      setkey(controls,.event,.ptid)
-      # Define cases in particular match-group
-      cases <- controls[.event==1]
-      setkey(cases,.ptid)
-      # If cases cannot become controls they are removed from controls
-      if (!reuseCases) controls <- controls[.event==0]
-      #find lengths of controls and cases
-      Tcontrols<-dim(controls)[1]
-      Ncases<-dim(cases)[1]
-      # sort controls by random number - so that they can be selected sequentially
-      set.seed(17)
-      controls[,random:=runif(.N,1,Ncontrols*10)]
-      setkey(controls,random)
-      # vectors for Rcpp
-      CONTROLS <- controls[,.ptid]
-      CASES <- cases[,.ptid]
-      if(!NoIndex){
-        controlIndex <- controls[,.controlIndex]
-        caseIndex <- cases[,.caseIndex]
-      } else {
-        controlIndex <- 0L
-        caseIndex <- 0L
-      }
-      Output <- data.table(heaven::Matcher(Ncontrols, Tcontrols, Ncases, NreuseControls,  
-                                           controlIndex, caseIndex, CONTROLS, CASES,noindex))    
-      Output
-    })) # end function and do.call
-    parallel::stopCluster(CLUST)
-  }  #end cores>1
-#browser()  
-  setnames(selected.controls,c("caseid",".ptid"))
-  selected.controls[,.event:=0]
-  setkey(alldata,.event)
-  cases <- alldata[.event==1]
-  cases[,caseid:=.ptid]
-  # Create final dataset with cases and controls
-  FINAL <- rbind(cases[,.(.ptid,caseid,.event)],selected.controls[,.(.ptid,caseid,.event)])
-  setkey(FINAL)
-  #output
-  setnames(FINAL,".ptid",ptid) #give the proper ptid back
-  # Ensure that original ptid is character
-  datt[,ptid:=as.character(ptid)]
-  datt[,event:=NULL]
-  FINAL <- merge(FINAL,datt,by=ptid)
-  FINAL[,c(".case","cterms"):=NULL] # remove cterms - aggregated terms
-  setkeyv(FINAL,c("caseid",".event"))
-  #Add relevant caseid to controls
-  if (!NoIndex) FINAL[,caseIndex:=caseIndex[.N],by=caseid]
-  setnames(FINAL,".event",event)
-  FINAL 
+    #browser()  
+    #ptid - pnr i DST id
+    #event 0/1 distinguishes cases from controls
+    #terms c(1,2,3) - list of vairables to match by
+    #dat - dataset with all variables
+    #Ncontrols - number of controls to provide
+    #reuseCases - T og F or NULL - can a case be a control prior to being a case?
+    #reuseControls - T or F or NULL
+    #caseIndex - Integer or date, date where controls must be prior
+    #controlIndex - Index date for controls
+    options(warn=-1)
+    # copy input data
+    datt <- copy(dat)
+    if (NoIndex) noindex <- 1L else noindex <- 0L # allows omitting index vectors
+    # Check data.table
+    if (!is.data.table(dat)) stop("data not data.table")
+    # combine matching variables to single term - cterms
+    datt[, cterms :=do.call(paste0,.SD),.SDcols=terms] 
+    # Select relevant part of table for matching
+    if (!NoIndex){
+        alldata <- datt[,.SD,.SDcols=c(ptid,caseIndex,controlIndex,event,"cterms") ]
+        setnames(alldata,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
+    }
+    else{
+        alldata <- datt[,.SD,.SDcols=c(ptid,event,"cterms") ]
+        setnames(alldata,c(".ptid",".event",".cterms"))
+    }
+    #Checking variables (partially)
+    numbers <- table(alldata[,.event])
+    if  ((names(numbers)==c("0","1")) !=c(T,T)) stop (" Event not 0 or 1 ")
+    # prepare to split and split
+    setkey(alldata,.cterms)
+    split.alldata <- split(alldata,by=".cterms")
+    if (cores<2){
+        # Prepare progress bar
+        totalprogress <-as.numeric(length(split.alldata)/1000)
+        pb <-txtProgressBar(min = 0, max = 1000, initial = 0, char = "=",
+                            width = NA, title, label, style = 1, file = "")
+        progress <- 0;
+        # Select controls - rbind of each split-member that selects controls
+        selected.controls <- do.call("rbind",lapply(split.alldata,function(controls){
+            #browser()    
+            # Setnames because data.table called from function
+            if (!NoIndex) setnames(controls,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
+            else setnames(controls,c(".ptid",".event",".cterms"))
+            setkey(controls,.event,.ptid)
+            # Define cases in particular match-group
+            cases <- controls[.event==1]
+            setkey(cases,.ptid)
+            # Uf cases cannot becom controls they are removed from controls
+            if (!reuseCases) controls <- controls[.event==0]
+            #find lengths of controls and cases
+            Tcontrols<-dim(controls)[1]
+            Ncases<-dim(cases)[1]
+            # sort controls by random number - so that they can be selected sequentially
+            set.seed(17)
+            controls[,random:=runif(.N,1,Ncontrols*10)]
+            setkey(controls,random)
+            # vectors for Rcpp
+            CONTROLS <- as.character(controls[,.ptid])
+            CASES <- as.character(cases[,.ptid])
+            NreuseControls <- as.numeric(reuseControls) # Integerlogic for Rcpp
+            if(!NoIndex){
+                controlIndex <- controls[,.controlIndex]
+                caseIndex <- cases[,.caseIndex]
+            } else {
+                controlIndex <- 0L
+                caseIndex <- 0L
+            }
+            Output <- heaven::Matcher(Ncontrols, Tcontrols, Ncases, NreuseControls,  
+                                      controlIndex, caseIndex, CONTROLS, CASES,noindex)
+            setDT(Output)
+            progress <<- progress+1/totalprogress
+            #Progress bar
+            setTxtProgressBar(pb,progress)
+            flush(stdout())
+            Output
+        })) # end function and do.call
+    } #end if cores<2
+    else {
+        CLUST <- parallel::makeCluster(min(parallel::detectCores(),cores))
+        print(CLUST)
+        parallel::clusterExport(CLUST, c("Matcher"))#,"NreuseControls","noindex","reuseCases","NoIndex"),envir=environment())
+        ## parallel::clusterEvalQ(CLUST, library(data.table))
+        ## parallel::clusterEvalQ(CLUST, library(heaven))
+        # Select controls - rbind of each split-member that selects controls
+        selected.controls <- do.call(rbind,foreach::foreach(controls=split.alldata,.packages=c("heaven"),.export=c("reuseControls")) %dopar% {
+            ## selected.controls <- do.call(rbind,parallel::parLapply(CLUST,split.alldata,function(controls){
+            # Setnames because data.table called from function
+            if (!NoIndex) data.table::setnames(controls,c(".ptid",".caseIndex",".controlIndex",".event",".cterms"))
+            else data.table::setnames(controls,c(".ptid",".event",".cterms"))
+            data.table::setkey(controls,.event,.ptid)
+            # Define cases in particular match-group
+            cases <- controls[.event==1]
+            data.table::setkey(cases,.ptid)
+            # If cases cannot become controls they are removed from controls
+            if (!reuseCases) controls <- controls[.event==0]
+            #find lengths of controls and cases
+            Tcontrols<-dim(controls)[1]
+            Ncases<-dim(cases)[1]
+            # sort controls by random number - so that they can be selected sequentially
+            set.seed(17)
+            controls[,random:=runif(.N,1,Ncontrols*10)]
+            setkey(controls,random)
+            # vectors for Rcpp
+            CONTROLS <- controls[,.ptid]
+            CASES <- cases[,.ptid]
+            NreuseControls <- as.numeric(reuseControls) # Integerlogic for Rcpp
+            if(!NoIndex){
+                controlIndex <- controls[,.controlIndex]
+                caseIndex <- cases[,.caseIndex]
+            } else {
+                controlIndex <- 0L
+                caseIndex <- 0L
+            }
+            ## print(list(Ncontrols, Tcontrols, Ncases, NreuseControls, controlIndex, caseIndex, CONTROLS, CASES,noindex))
+            Output <- heaven::Matcher(Ncontrols, Tcontrols, Ncases, NreuseControls,  
+                                      controlIndex, caseIndex, as.character(CONTROLS), as.character(CASES),noindex)
+            setDT(Output)
+            Output
+        }) # end function and do.call
+        parallel::stopCluster(CLUST)
+        setDT(selected.controls)
+    }  #end cores>1
+    setnames(selected.controls,c("caseid",".ptid"))
+    selected.controls[,.event:=0]
+    setkey(alldata,.event)
+    cases <- alldata[.event==1]
+    cases[,caseid:=.ptid]
+    # Create final dataset with cases and controls
+    FINAL <- rbind(cases[,.(.ptid,caseid,.event)],selected.controls[,.(.ptid,caseid,.event)])
+    setkey(FINAL)
+    #output
+    setnames(FINAL,".ptid",ptid) #give the proper ptid back
+    # Ensure that original ptid is character
+    datt[,ptid:=as.character(ptid)]
+    datt[,event:=NULL]
+    FINAL <- merge(FINAL,datt,by=ptid)
+    FINAL[,c(".case","cterms"):=NULL] # remove cterms - aggregated terms
+    setkeyv(FINAL,c("caseid",".event"))
+    #Add relevant caseid to controls
+    if (!NoIndex) FINAL[,caseIndex:=caseIndex[.N],by=caseid]
+    setnames(FINAL,".event",event)
+    FINAL 
 }
