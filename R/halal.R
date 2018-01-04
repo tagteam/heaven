2b02f4c4d6cf7026e5b7faeb4c1733cf263979b1
Regitze Kuhr Skals / Region Nordjylland <bz9v@aas.its.nja.dk>
2018-01-04 10:56:57 +0100

    halal ændring

--- R/halal.R
+++ R/halal.R
@@ -34,8 +34,9 @@
 #' icd_codes <- sample(numbers, 500000, replace = T)
 #' diag <- paste(abc,icd_codes,sep='')
 #' diag <- paste('D',diag,sep='')
-#' inddto <- c(11688:14974)
-#' inddto <- as.Date(sample(inddto,500000,replace=T),origin='1970-01-01')
+#' inddto.nr <- c(11688:14974)
+#' inddto <- as.Date(sample(inddto.nr,500000,replace=T),origin='1970-01-01')
+#' index <- as.Date(sample(inddto.nr,500000,replace=T),origin='1970-01-01')
 #' uddto <- c(1:10)
 #' uddto <- inddto+sample(uddto,500000,replace = T)
 #' pattype <- sample(c(1:3),500000,replace = T)
@@ -71,33 +72,60 @@
 #' @export
 #' @author Regitze Kuhr Skals <r.skals@rn.dk>
 
-halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.out=NULL,mult=FALSE,
-                  first.pnr=FALSE,pat=NULL,prefix='',entryvar='inddto',id='pnr',outvar='uddto',codevar='diag',patvar='pattype',record.id='recnum'){
+extractIcd <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.out=NULL,mult=FALSE,
+                  first.pnr=FALSE,pat=NULL,prefix='',entryvar='inddto',id='pnr',outvar='uddto',codevar='diag',
+                  patvar='pattype',record.id='recnum',indexvar=NULL,index.int=NULL,outcome=FALSE){
   
   # HALAL definition of diseases
   
-  halal_defs <- list(af='I48',rheum_heart_valve_disease=c(as.character(c(4240,4241,39500:39502,39508,39509,39600:39604,39608,39609)),'Z952','Z954','I05','I06','I080A','I082A','I083A'),
-                     dvt_pe=paste('I',as.character(c(801:803,808,809,821:823,828,829,26)),sep=''),
-                     stroke_tci_systemic_embolism_thrombosis=c('I63','I64','I60','I61','I62','S064','S065','S066','G458','G459','I74'),
-                     bleeding_without_hemstroke=c('I312','N02','R31','R04','H313','H356','H431','H450','H052A','K228F','K250','K252',
-                                                  'K254','K256','K260','K262','K264','K266','K270','K272','K274','K276','K280','K282',
-                                                  'K284','K286','K290','K298A','K625','K638B','K638C','K661','K838F','K868G','K920','K921','K922',
-                                                  'KI850','I864A','S064','S065','S066','G951A','S368D','J942','D500','D62'),
-                     cardiomyopathy='I42',
-                     hf=c('I50','I110'),
-                     lung_edema='J81',
-                     HF=c('I42','I50','I110','J81'),
-                     ihd=paste('I',as.character(c(20:25)),sep=''),
-                     ap='I20',
-                     ami=c('I21','I22'),
-                     perart='I70',
-                     cancer='C',
-                     chronic_kidney_disease=c('N02','N03','N04','N05','N06','N07','N08','N11','N12','N14','N18','N19','N26','N158','N159',
-                                              'N160','N162','N163','N164','N168','Q61','E102','E112','E132','E142','I120','M321B'),
-                     copd=c('J42','J43','J44'),
-                     liver_disease=c('B15','B16','B17','B18','B19','C22','K70','K71','K72','K73','K74','K75','K76','K77','Z944','I982','D684C'),
+  disease_defs <- list(af=c("DI48","42793", "42794"),rheumvalve=c(as.character(c(39400, 39401, 39402, 
+                      39408, 39409, 39500, 39501, 39502, 39508, 39509, 39600, 39601, 39602, 39603, 39604, 39608, 39609,
+                      99751, 4240, 4241)),paste0('D',c('Z952','Z954','I05','I06','I080A','I081A','I082A','I083A'))),
+                     dvtpe=paste('DI',as.character(c(801:803,808,809,821:823,828,829,26)),sep=''),
+                     stroketci=c(as.character(c(43309, 43399, 43409, 43499, 43600, 43601, 43609, 43690, 43699, 43700,
+                                               43701, 43708, 43709, 43790, 43791, 43798, 43799, 43809, 43899)),
+                     paste0('D',c('I63','I64','G458','G459'))),
+                     bleeding=c(as.character(c(85200, 85201, 85202, 85203, 85209, 85210, 85211, 85212, 85290, 85291, 
+                                               85292, 43008, 43009, 43098, 43099, 43100, 43101, 43108, 43109, 43190,
+                                               43191, 43198, 43199, 28000, 45601, 53098, 53190, 53192, 53195, 53290, 
+                                               53390, 53490, 53501, 56319, 56904, 56915)),paste0('D',
+                     c('I60','I61','I62','I312','N02','R31','R04','H313','H356','H431','H450','H052A','K228F','K250',
+                       'K252','K254','K256','K260','K262','K264','K266','K270','K272','K274','K276','K280','K282',
+                                                  'K284','K286','K290','K298A','K625','K638B','K638C','K661','K838F',
+                       'K868G','K920','K921','K922','KI850','I864A','S064','S065','S066','G951A','S368D','J942','D500'
+                       ,'D62'))),
+                     cardiomyopathy='DI42',
+                     lungedema='DJ81',
+                     hf=c('DI42','DI50','DI110','DJ81',as.character(c(42599, 42709, 42710, 42711, 42719))),
+                     ihd=c(paste('DI',as.character(c(20:25)),sep=''),as.character(c(41009, 41099, 41109, 41199, 41209,
+                                                                        41299, 41309, 41399, 41409, 41499))),
+                     mi=c('DI21','DI22',as.character(c(41009, 41099))),
+                     perart=c('DI70',as.character(c(44009, 44019, 44020, 44021, 44028, 44029, 44030, 44039, 44099))),
+                     cancer=c(paste0('DC0',0:9),paste0('DC',10:97),as.character(c(109:140))),
+                     ckd=c(paste0('D',c('N02','N03','N04','N05','N06','N07','N08','N11','N12','N14',
+                                              'N18','N19','N26','N158','N159','N160','N162','N163','N164','N168','Q61',
+                                              'E102','E112','E132','E142','I120','M321B','Q612','Q613','Q615','Q619')),
+                                              as.character(c(24902, 25002,58200,
+                                               58201, 58202, 58208, 58209, 58300, 58301, 58302, 58308, 58309, 58499, 
+                                               59009, 59320, 75310, 75311, 75319, 79299, 40399, 40499, 44609, 44629))),
+                     copd=c('DJ42','DJ43','DJ44',as.character(c(49100, 49101, 49102, 49103, 49104, 49108, 49109, 49200,
+                                                                49201, 49208, 49209))),
+                     liver=c(paste0('D',c('B15','B16','B17','B18','B19','C22','K70','K71','K72','K73','K74',
+                                                  'K75','K76','K77','Z944','I982','D684C','Q618A')),
+                                     as.character(c(15509, 15519, 15589, 45600, 45601, 57109, 57110, 57111, 57119, 
+                                                    57190, 57191, 57192, 57193, 57194, 57199, 57200, 57201, 57209, 
+                                                    57300, 57301, 57302, 57303, 57304, 57305, 57309))),
                      
-                     alcohol=c('F10','K70','E52','T51','K860','E244','G312','I426','O354','Z714','Z721','G621','G721','K292','L278A'))
+                     alcohol=c(paste0('D',c('F10','K70','E52','T51','K860','E244','G312','I426','O354','Z714','Z721',
+                                            'G621','G721','K292','L278A')),'E8609',
+                               as.character(c(97909, 97919, 97929, 97939, 97949, 97959, 98009, 98019, 98029,
+                                              98099, 29109, 29119, 29129, 29139, 29199, 30309, 30319, 30320, 30328,
+                                              30329, 30390, 30391, 30399, 57710, 57109, 57110))),
+                     dm=c(paste0('DE',10:14),'250'),
+                     ht=c(paste0('DI',10:15),as.character(c(40009, 40019, 40029, 40039, 40099, 40199, 40299,
+                                                                      40399, 40499))),
+                     ulcus=paste0('DK',25:27)
+                     )
   
   ##  Make into data.table and change relevant variable names to lower case
   d <- as.data.table(dat)
@@ -111,6 +139,10 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
   setnames(d,patvar,'pattype')
   setnames(d,record.id,'recnum')
   
+  if(!is.null(indexvar)){
+    setnames(d,indexvar,'index')
+  }
+  
   if(!is.null(p.in) & class(p.in)!='Date'){
     stop('p.in is not a date')    
   }
@@ -119,24 +151,24 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
     stop('p.out is not a date')    
   }
   
-  ## Substraction of diagnoses
+  ## extraction of diagnoses
   if(!is.null(disease)){
-    diags_for_substraction <- unlist(halal_defs[disease])
+    diags_for_extraction <- unlist(disease_defs[disease])
     if(!is.null(inclusions)){
-      diags_for_substraction <- c(diags_for_substraction,inclusions)
+      diags_for_extraction <- c(diags_for_extraction,inclusions)
     }
   }
   else{
-    diags_for_substraction <- inclusions
+    diags_for_extraction <- inclusions
   }
   
   setkey(d,diag) #Sort by key
   icdcodes <- d[.(unique(diag)),.(diag),mult="first"] #unique diagnoses
-  icdcodes <- icdcodes[unlist(lapply(paste('^D?',diags_for_substraction,sep=''),grep,diag))] #the unique diagnoses of interest
+  icdcodes <- icdcodes[unlist(lapply(paste('^?',diags_for_extraction,sep=''),grep,diag))] #the unique diagnoses of interest
   if(!is.null(exclusions)){
-    ex.diag <- grep(paste(paste('^D?',exclusions,sep=''),collapse='|'),icdcodes$diag)
+    ex.diag <- grep(paste(paste('^?',exclusions,sep=''),collapse='|'),icdcodes$diag)
     icdcodes <- icdcodes[-ex.diag]
-    diags_for_substraction <- setdiff(diags_for_substraction,exclusions)
+    diags_for_extraction <- setdiff(diags_for_extraction,exclusions)
   }
   out <- d[icdcodes,nomatch = 0L] #Patients with unique diagnoses of interest
   
@@ -151,6 +183,16 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
   
   if(is.null(p.in)&!is.null(p.out)){
     out <- out[p.out>inddto]
+  }
+  
+  # Find diagnoses in specific interval before an index date
+  if(!is.null(index)&!is.null(index.int)){
+    out <- out[index>=inddto&(index-index.interval)<=inddto]
+  }
+  
+  # Find diagnoses after an index date (outcome)
+  if(!is.null(index)&outcome==TRUE){
+    out <- out[index<inddto]
   }
   
   #Restrict to specific type of patient (pattype)
@@ -170,10 +212,10 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
     
     # Mark different diseases
     out[,dis:=""]
-    names_dis <- names(halal_defs[disease])
-    sygdomme <- halal_defs[disease]
+    names_dis <- names(disease_defs[disease])
+    sygdomme <- disease_defs[disease]
     
-    idx <- lapply(sygdomme,function(var){grep(paste(paste('^D?',unlist(var),sep=''),collapse='|'),out$diag)})
+    idx <- lapply(sygdomme,function(var){grep(paste(paste('^?',unlist(var),sep=''),collapse='|'),out$diag)})
     
     for(x in seq_along(idx)){
       set(out,i=idx[[x]],j='dis',value=names_dis[x])
@@ -193,10 +235,10 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
     
     # Mark different diseases
     out[,dis:=""]
-    names_dis <- names(halal_defs[disease])
-    sygdomme <- halal_defs[disease]
+    names_dis <- names(disease_defs[disease])
+    sygdomme <- disease_defs[disease]
     
-    idx <- lapply(sygdomme,function(var){grep(paste(paste('^D?',unlist(var),sep=''),collapse='|'),out$diag)})
+    idx <- lapply(sygdomme,function(var){grep(paste(paste('^?',unlist(var),sep=''),collapse='|'),out$diag)})
     
     for(x in seq_along(idx)){
       set(out,i=idx[[x]],j='dis',value=names_dis[x])
@@ -218,6 +260,10 @@ halal <- function(dat,disease=NULL,inclusions=NULL,exclusions=NULL,p.in=NULL,p.o
   setnames(out,'pattype',patvar)
   setnames(out,'recnum',record.id)
   
-  return(list(data=out,diagnoses=diags_for_substraction,unique.icd=icdcodes$diag))
+  if(!is.null(indexvar)){
+    setnames(out,'index',indexvar)
+  }
+  
+  return(list(data=out,diagnoses=diags_for_extraction,unique.icd=icdcodes$diag))
 }
 
