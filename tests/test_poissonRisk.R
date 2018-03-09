7a12a8a2187483dbdf992b447f50a7292305fea1
Regitze Kuhr Skals / Region Nordjylland <bz9v@aas.its.nja.dk>
2018-03-09 13:42:38 +0100

    test program for poissonRisk adjusted to package testthat

--- tests/test_poissonRisk.R
+++ tests/test_poissonRisk.R
@@ -11,6 +11,7 @@ library(doParallel)
 # if data is without censoring, and only theta0 is included in simulated data, rates
 # in glm should match the baselinerates we used for construction of simulated data
 
+test_that("Simulated rates vs. estimated rates",{
 set.seed(28587)
 simdat <- simpoisson(N=1000,
                      changepoints=c(1,2),
@@ -25,24 +26,27 @@ simdat <- simpoisson(N=1000,
 
 fit <- poissonregression(formula=event~-1+X+Z+time+offset(log(risktime)),data=simdat,
                          timegrid=c(1,2))
-fit1 <- poissonregression(formula=event~-1+X*Z+time+offset(log(risktime)),data=simdat,
-                         timegrid=c(1,2))
 
 # Rates simulated: 0.005, 0.02
 testdat <- aggregateData(simdat,1:2)
 
-# Relative frequencies 
+## Relative frequencies 
 
 # First time interval
-testdat[X==0&Z==0&time==1,event]/testdat[X==0&Z==0&time==1,risktime]
+rate1 <- testdat[X==0&Z==0&time==1,event]/testdat[X==0&Z==0&time==1,risktime]
 
 # Second timeinterval
-testdat[X==0&Z==0&time==2,event]/testdat[X==0&Z==0&time==2,risktime]
+rate2 <- testdat[X==0&Z==0&time==2,event]/testdat[X==0&Z==0&time==2,risktime]
+
+expect_equal(as.numeric(exp(fit$coefficients[3])),rate1,tolerance=.0002)
+expect_equal(as.numeric(exp(fit$coefficients[4])),rate2,tolerance=.002)
+})
 
 ############################################################################
 # Does rates act as expected when beta is included in simulated data
 ############################################################################
 
+test_that('Decrease and increase in rate by beta',{
 test.beta <- function(b,theta0=rep(0.005,2),n=10000,ints=c(1:2),time.p=1){
 simdat <- simpoisson(N=n,
                      changepoints=ints,
@@ -61,19 +65,17 @@ return(list(exp(coef(fit)),poissonRisk(fit,interval=1,
             newdata=expand.grid(X=0:1,Z=0:1,time=factor(time.p,levels=ints)))))
 }
 
-# reference
-test.beta(rep(1,2))[[2]]
+expect_true(test.beta(rep(1,2))[[2]][2,4]>test.beta(rep(0.5,2))[[2]][2,4])
 
-# decreased effect of beta is expected
-test.beta(rep(0.5,2))[[2]]
+expect_true(test.beta(rep(1,2))[[2]][2,4]<test.beta(rep(2,2))[[2]][2,4])
 
-# increased effect of beta is expected
-test.beta(rep(2,2))[[2]]
+})
 
 ######################################
 # Test interactions
 ######################################
 
+test_that('Interactions',{
 fita <- poissonregression(formula=event~-1+X+Z+time+offset(log(risktime)),data=simdat,
                          timegrid=c(1,2))
 fita1 <- poissonregression(formula=event~-1+X*Z+time+offset(log(risktime)),data=simdat,
@@ -81,18 +83,20 @@ fita1 <- poissonregression(formula=event~-1+X*Z+time+offset(log(risktime)),data=
 
 a <- poissonRisk(fita,interval=1,
                  newdata=expand.grid(X=0:1,Z=0:1,time=factor(1,levels=c(1,2))))
-a[2,4]/a[1,4]
-a[4,4]/a[3,4]
+
+expect_equal(a[2,4]/a[1,4],a[4,4]/a[3,4],tolerance=0.002)
 
 a1 <- poissonRisk(fita1,interval=1,
                   newdata=expand.grid(X=0:1,Z=0:1,time=factor(1,levels=c(1,2))))
-a1[2,4]/a1[1,4]
-a1[4,4]/a1[3,4]
+
+expect_true(round(a1[2,4]/a1[1,4],2)!=round(a1[4,4]/a1[3,4],2))
+})
 
 ###############################################
 # Test if model can be fitted without offset
 ###############################################
 
+test_that('exclude offset',{
 simdat <- simpoisson(N=1000,
                      changepoints=c(1,2),
                      baseline=c(0.005,0.02),
@@ -106,32 +110,34 @@ simdat <- simpoisson(N=1000,
 fit.test <- poissonregression(formula=event~-1+X+Z+time,data=simdat,
                                      timegrid=c(1,2))
 
-poissonRisk(fit.test,interval=1,
-            newdata=expand.grid(X=0:1,Z=0:1,time=factor(1,levels=c(1,2))))
+expect_error(poissonRisk(fit.test,interval=1,
+            newdata=expand.grid(X=0:1,Z=0:1,time=factor(1,levels=c(1,2)))))
+})
 
 ######################################
 # Increase number of intervals
 ######################################
 
-t1 <- test.beta(b=rep(1,length(c(15:50))),
-          n=10000,theta0 = rep(0.005,length(c(15:50))),
-          ints=c(15:50),
-          time.p=30)
-
-t1[[2]]
-
-# Add effect of X
-t1 <- test.beta(b=rep(3,length(c(15:50))),
-                n=10000,theta0 = rep(0.005,length(c(15:50))),
-                ints=c(15:50),
-                time.p=35)
-
-t1[[2]]
+# t1 <- test.beta(b=rep(1,length(c(15:50))),
+#           n=10000,theta0 = rep(0.005,length(c(15:50))),
+#           ints=c(15:50),
+#           time.p=30)
+# 
+# t1[[2]]
+# 
+# # Add effect of X
+# t1 <- test.beta(b=rep(3,length(c(15:50))),
+#                 n=10000,theta0 = rep(0.005,length(c(15:50))),
+#                 ints=c(15:50),
+#                 time.p=35)
+# 
+# t1[[2]]
 
 ##################################################################
 # Testing standard errors from lava (robust vs. non-robust)
 ##################################################################
 
+test_that('robust vs. non robust sd',{
 # Test function with se option in lava estimate added
 poissonRisk.test <- function(model,interval,newdata,se.robust){
   model.t <- terms(model)
@@ -193,3 +199,7 @@ sd(est.sim) # standard deviation of absolute risks based on 3000 simulated datas
 est.lava[,5] # robust se lava
 est.lava2[,5] # non-robust se lava
 
+expect_equal(est.lava2[,5],sd(est.sim),tolerance=0.0003)
+expect_equal(est.lava[,5],sd(est.sim),tolerance=0.0003)
+
+})
