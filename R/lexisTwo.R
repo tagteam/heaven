1e2be85d46e2e28d84a202150d9d936365c76cbf
Thomas Alexander Gerds <tag@biostat.ku.dk>
2018-08-10 09:01:12 +0200

    improved help page

--- R/lexisTwo.R
+++ R/lexisTwo.R
@@ -1,16 +1,19 @@
 #' @title lexisTwo
 #' 
-#' @description 
+#' @description
+#' Splitting is about collecting person specific exposure-outcome-confounder
+#' pattern over time in start-stop-event format.
 #' lexixTwo is one of 3 splitting functions in heaven.
-#' This particular function (lexixTwo) is useful to add
-#' the time-dynamic occurrence of comorbidities and other events
-#' to an existing data set which contains person specific information
-#' about periods in time. The person specific time periods are split 
-#' according to the occurrence dates of the comorbidities and other events.
+#' lexixTwo is useful to add the time-dynamic information about comorbidities
+#' and other events in binary (yes/no) format 
+#' to an existing data set which readily contains person specific information
+#' in start-stop-event format. A person specific time interval (start-stop) of the existing data
+#' set is split according to the occurrence dates of the comorbidities and other
+#' events whenever the comorbidity status (event status) changes within the time interval.
 #' 
 #' The "base" data are the data to be split. They 
 #' may contain much information, but the key is "id","start","end" and "event". 
-#' These describe the participant's id, start of time period, end of time period 
+#' These describe the participant's id, start of time interval, end of time interval
 #' and the event of interest (must be 0/1).
 #' 
 #' The other input is a data.table with the splittingguide. This data.table 
@@ -24,14 +27,22 @@
 #' @author Christian Torp-Pedersen
 #' @param indat A data.table or data.frame whose first 4 columns are in that order:
 #' \itemize{
-#' \item{id}{person identification variable such as PNR. The data may contain multiple lines per subject.}
-#' \item{start}{Start of time period. Either in date format or given as numeric/integer.}
-#' \item{end}{End of time period. Either in date format or given as numeric/integer.}
-#' \item{event}{Event}
+#' \item{id}{ Person identification variable such as PNR. The data may contain multiple lines per subject.}
+#' \item{start}{ Start of time interval. Either a date or an integer/numeric.}
+#' \item{end}{ End of time interval. Either in date format or given as numeric/integer.}
+#' \item{event}{ Binary 0-1 variable indicating if an event occurred at end of interval}
 #' }
-#' @param splitdat - Data with splitting guide - id and columns with dates to 
-#' split by 
-#' @param invars - vector of column names for id/entry/exit/event - in that 
+#' @param splitdat The splittingguide. A data.table which contains person specific
+#'                 information about the onset dates of comorbidities and other events.
+#' \itemize{
+#' \item{id}{ Person identification variable such as PNR. The data may contain multiple lines per subject.}
+#' \item{Date 1}{ Either a date or an integer/numeric. Format must match that of the start and stop of arguments \code{indat}.
+#'                The onset date of comorbidity 1 or other event.
+#'                If integer/numeric it can be time since a baseline date on project specific scale (e.g., days or months).}
+#' \item{Date 2}{ Either a date or an integer/numeric. The onset date of comorbidity 2 or other event.
+#'                If integer/numeric it can be time since a baseline date on project specific scale (e.g., days or months).}
+#' }
+#' @param invars vector of column names for id/entry/exit/event - in that 
 #' order, example: c("id","start","end","event")
 #' @param splitvars - vector of column names of columns containing dates to 
 #' split by. example: c("date1","date2","date3","date4")
@@ -43,12 +54,12 @@
 #' data, but now they have the value zero before the dates and 1 after.
 #' @export
 #' @details 
-#' #' The program checks that intervals are not negative. Violation stops  
-#' with error. Overlaps may occur in real situations, but the user needs to make 
+#' The program checks that intervals are not negative. Violation results in
+#' an error. Overlap may occur in real data, but the user needs to make 
 #' decisions regarding this prior to using this function.
 #' 
 #' It is required that the splittingguide contains at least one record.  
-#' Missing data for key variables are not allowed and will cause errors.
+#' Missing data in the person id variables are not allowed and will cause errors.
 #' 
 #' A note of caution: This function works with dates as integers. R has a default
 #' original of dates as 1 January 1970, but other programs have different
@@ -61,22 +72,25 @@
 #' quence when multiple splits are made on the same day. When there is an event
 #' on a period with zero length it is important to keep that period not to 
 #' loose events for calculations. Whether other zero length records should be
-#' kept in calculations depend on context.
+#' kept in calculations depends on the context.
 #' @seealso lexisSeq lexisFromTo
 #' @examples
 #' library(data.table)
-#' dat <- data.table(id=c("A","A","B","B","C","C","D","D"),
+#' 
+#' dat <- data.table(pnr=c("123456","123456","234567","234567","345678","345678","456789","456789"),
 #'                 start=c(0,100,0,100,0,100,0,100),
 #'                 end=c(100,200,100,200,100,200,100,200),
 #'                 event=c(0,1,0,0,0,1,0,1))
-#' split <- data.table (id=c("A","B","C","D"),date1=c(0,50,150,300),
-#'                  date2=c(25,75,175,325),date3=c(30,30,30,30),
-#'                     date4=c(0,1,0,1))
-#' temp <- lexisTwo(dat # inddato with id/in/out/event
+#' split <- data.table (pnr=c("123456","234567","345678","456789"),como1.onset=c(0,50,150,300),
+#'                  como2.onset=c(25,75,175,325),como3.onset=c(30,30,30,30),
+#'                     como4.onset=c(0,1,0,1))
+#' lexisTwo(dat # inddato with id/in/out/event
 #'        ,split # Data with id and dates
 #'        ,c("id","start","end","event") #names of id/in/out/event - in that order
-#'        ,c("date1","date2","date3","date4")) #Names var date-vars to split by
-#' temp
+#'        ,c("date1","date2","date3","date4")) #Names of date-vars to split by
+#' 
+#'
+#' 
 #' @export
 lexisTwo <- function(indat # inddato with id/in/out/event - and possibly other variables
                     ,splitdat # Data with id and dates
