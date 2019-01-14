804d6bf37178214f6b0038edafd8719dca5bad11
Thomas Alexander Gerds <tag@biostat.ku.dk>
2019-01-14 09:08:44 +0100

    now documenting Framingham the R extension way

--- R/Framingham.R
+++
@@ -1,9 +0,0 @@
-#' Documentation for data Framingham
-#' 
-#' The Framingham dataset is a public version of several examinations of people
-#' in the Framingham study.  Various variables have been adjusted to ensure
-#' anonymity of people. Thus, the data cannot be used for science, only demon-
-#' stration.
-#' 
-#' Details of variables can be found in the inst/doc folder: framdoc.pdf
-"Framingham"
