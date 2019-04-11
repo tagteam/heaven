8db790d8744ff837b574543baf46484821b65894
Thomas Alexander Gerds <tag@biostat.ku.dk>
2019-04-11 08:39:46 +0200

    fixing

--- sandbox/test-mm.R
+++ sandbox/test-mm.R
@@ -12,7 +12,7 @@ lpr=simAdmissionData(N)
 R03 = list(atc=c("R03AK11"),
            maxdepot=10000,
            period=as.Date(c("1995-01-01", "2012-12-31")),
-           prescriptionwindow=2,
+           prescriptionwindow=5,
            doses=list(value=c(5,10),
                       min = c(1,1),
                       max = c(2,2),
@@ -38,12 +38,9 @@ lmdb1 <- lmdb[pnr==1][1:6]
 ## lmdb1 <- lmdb[pnr==1][1:5]
 ## lmdb1[2,eksd:=as.Date("1995-01-01")+15]
 a <- medicinMacro(drugs=list("R03"=R03),drugdb=lmdb1,admdb=lpr)
-
-A <- medicinMacro(drugs=list("R03"=R03),drugdb=lmdb1,admdb=lpr,collapse=0L)
-A
 B <- a$R03[dose>0]
 b <- xrecepter(drugdb=lmdb1,adm=lpr,window=5,remote=TRUE,remote.home="/home/ifsv/grb615",wd="~/tmp/",save.tmp=FALSE,value=rep(R03$doses$value,2),min=rep(R03$doses$min,2),max=rep(R03$doses$max,2),def=rep(R03$doses$def,2),maxdepot=100,period=c(12784,20089),atc="R03AK11",name="blaupill",sas.program="/usr/local/bin/sas",server="doob",user="grb615")
-B
+a$R03[dose>0]
 b
 
 ## Tasks:
