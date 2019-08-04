32470ab222d13325d7c8d7a6ad6985be24f91de2
Christian Torp-Pedersen <ctp@heart.dk>
2019-08-04 21:29:19 +0200

    Exclusions added to findConditions

--- R/findConditions.R
+++ R/findConditions.R
@@ -23,26 +23,32 @@
 #' The function is designed to search a group of variables (character) for 
 #' multiple conditions defined in a list of named character vectors. The func-
 #' tion will produce a data.table with selected variables for cases where a
-#' match is found.
+#' match is found.  In addition a list of names character vectors can have
+#' exclusions from the search.  This last facility is useful if e.g. all
+#' cancer except non melanoma skin cancer is sought.  In that case inclusion 
+#' can have all cancers and the exclusions just the non-melanoma skin cancer.
 #' 
 #' See examples for common use of the output
 #' @usage
-#' findCondition(data, vars, keep, conditions, match="contain",condition.name)
+#' findCondition(data, vars, keep, conditions, exclusions, 
+#' match="contain",condition.name)
 #' 
 #' @param data Data in which to search for conditions
 #' @param vars Name(s) of variable(s) in which to search.
 #' @param keep a character vector of the columns in Data.table to keep in
 #' output
 #' @param conditions A named list of (vectors of) search strings. See examples.
+#' @param exclusions A names list of (vectors of) search strings to exclude
+#' from the output.
 #' @param match A variable to tell how to use the character vectors:
 #' "exact"=exactly matches the search string, "contains"=contains the search
 #' string, "starts"=Starts with the search string, "end"=Ends with the search
 #' string
-#' @param condition.name Name of variable(s) where values define conditions. The values
-#' of this variable are the names from parameter "conditions".
+#' @param condition.name Name of variable(s) where values define conditions. 
+#' The values of this variable are the names from parameter "conditions".
 #' @return
-#' A data table that includes the "keep-variables" and a variable named \code{condition.name} which
-#' identifies the condition searched for
+#' A data table that includes the "keep-variables" and a variable named 
+#' \code{condition.name} which #' identifies the condition searched for
 #' @examples
 #' library(heaven)
 #' library(data.table)
@@ -53,9 +59,12 @@
 #' )
 #' search <- list(Cond1=c('A1','A2'),Cond2=c('B10','B40','B5'),
 #' Cond3=c('A1','C20','B2'))
+#' 
+#' excl <- list(Cond2='B100')
 #'
 #' out <- findCondition(opr,vars=c("opr","oprtil"),keep=c("pnr","odto"),
-#'         conditions=search,match="start",condition.name="cond")
+#'         conditions=search, exclusions=excl,
+#'         match="start",condition.name="cond")
 #' ### And how to use the result:
 #' # Find first occurence of each condition and then use "dcast" to create
 #' # a data.table with vectors corresponding to each condition.
@@ -72,49 +81,77 @@
 #' # Whether to convert NAs to zero depends on the situation
 #' @author Christian Torp-Pedersen  <ctp@heart.dk>, Thomas A. Gerds <tag@biostat.ku.dk>
 #' @export
-findCondition <- function(data,
-                          vars,
-                          keep,
-                          conditions,
-                          match="contain",
-                          condition.name="X"){
-  cond=NULL
-  match <-match.arg(match,c("exact","contains","start"))
-  if (!is.character(vars) | !is.character(keep)) stop ("Error -  vars or keep not character")  
-  if (!class(conditions)=="list") stop ("Error - Conditions not a list")
+findCondition <- function (data, vars, keep, conditions,exclusions=NULL, match = "contain", 
+                           condition.name = "X") 
+{
+  cond = NULL
+  match <- match.arg(match, c("exact", "contains", 
+                              "start","end"))
+  if (!is.character(vars) | !is.character(keep)) 
+    stop("Error -  vars or keep not character")
+  if (!class(conditions) == "list" | is.null(names(conditions))) 
+    stop("Error - Conditions not a named list")
+  if(!is.null(exclusions) & (!class(conditions) == "list" | is.null(names(conditions))))
+    stop("Error - Exclusions should be NULL or a named list") 
   requireNamespace("data.table")
   setDT(data)
-  data <-copy(data)
-  for (variable in unique(c(vars,keep))){
-    if (!variable %in% names(data)) stop(paste0("Error - ",variable," not in data to be analysed"))
+  data <- copy(data)
+  for (variable in unique(c(vars, keep))) {
+    if (!variable %in% names(data)) 
+      stop(paste0("Error - ", variable, " not in data to be analysed"))
   }
   out <- NULL
-  for (i in 1:length(conditions)){
+  for (i in 1:length(conditions)) {
+    Ex <- FALSE # No exclusion list default
     cc <- conditions[[i]]
     name <- names(conditions)[[i]]
-    regexp <- switch(match,"exact"=paste0("^",cc,"$",collapse="|"),"contains"=paste0(cc ,collapse="|"),"start"=paste0("^",cc,collapse="|"))
-    if (length(vars)==1) {
-      found <- grepl(regexp,data[[vars]])
-    }else{
-      found <- rep(0L,NROW(data))
-      for (v in vars){
-        found <- found | grepl(regexp,data[[v]])                
+    regexp <- switch(match, exact    = paste0("^", cc,"$", collapse = "|"), 
+                     contains = paste0(cc,collapse = "|"), 
+                     start    = paste0("^",cc, collapse = "|"),
+                     end      = paste0("$",cc,collapse="|"))
+    if (!is.null(exclusions) & name %in% names(exclusions)){
+      ee <- exclusions[name]
+      regexpEx <- switch(match, 
+                         exact    = paste0("^", ee,"$",collapse = "|"), 
+                         contains = paste0(ee,collapse = "|"), 
+                         start    = paste0("^",ee, collapse = "|"),
+                         end      = paste0("$",ee,collapse="|"))
+      Ex <- TRUE 
+    }
+    if (length(vars) == 1) {
+      found <- grepl(regexp, data[[vars]])
+      if (Ex){
+        foundEx <- grepl(regexpEx,data[[vars]])
+        found <- found & !foundEx  
       }
     }
-    if (length(out)==0){
+    else {
+      found <- rep(0L, NROW(data))
+      for (v in vars) {
+        found <- found | grepl(regexp, data[[v]])
+      }
+      if (Ex){
+        foundEx <- rep(0L, NROW(data))
+        for (v in vars) {
+          foundEx <- foundEx | grepl(regexpEx, data[[v]])
+        }  
+      }
+      if (Ex) found <- found & !foundEx
+    }  
+    if (length(out) == 0) {
       out <- data[found]
-      out[,cond:=name]
-    } else{
+      out[, `:=`(cond, name)]
+    }
+    else {
       temp <- data[found]
-      temp[,cond:=name]
-      out <- rbindlist(list(out,temp))
+      temp[, `:=`(cond, name)]
+      out <- rbindlist(list(out, temp))
     }
   }
-  out <- out[,c("cond",keep),with=FALSE]
-  setnames(out,"cond",condition.name)
+  out <- out[, c("cond", keep), with = FALSE]
+  setnames(out, "cond", condition.name)
   out
 }
-
 
 ######################################################################
 ### findCondition.R ends here
