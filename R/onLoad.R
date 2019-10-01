c0279a43f87342711e54aed5a5dd424169bb1713
Thomas Alexander Gerds <tag@biostat.ku.dk>
2019-10-01 20:48:31 +0200

    lol

--- R/onLoad.R
+++ R/onLoad.R
@@ -4,6 +4,6 @@
     if (interactive()) {
         v = utils::packageVersion("heaven")
         packageStartupMessage(paste0("\nLOL: This is heaven! Version ", v))
-        packageStartupMessage(paste0("A good place to get started is vignette('user-heaven')."))
+        packageStartupMessage(paste0("A good place to get started is vignette('user-heaven').\nNu er det rigtigt!"))
     }
 }
