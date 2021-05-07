e37f2c08dd701b9446b99531fe8859574c7ce35e
ctpteam <ctp@heart.dk>
2021-05-08 00:03:15 +0200

    update importDREAM

--- R/importDREAM.R
+++ R/importDREAM.R
@@ -1,22 +1,24 @@
 #' @title importDREAM
 #' @description
 #' The DREAM dataset is a SAS dataset where all in the Danish population that
-#' have received public funding are recorded. A number of variables are presen-
-#' ted. For a detailed description of DREAM refer to www.dreammodel.dk/
+#' have received public funding are recorded. A number of variables are 
+#' presented. For a detailed description of DREAM refer to www.dreammodel.dk
+#' 
 #' The current function can output 2 datasets from the DREAM database 
 #' \itemize{
-#' \item{support}{A long version data.table providing a record per week for all 
+#' \item{support}{  A long version data.table providing a record per week for all 
 #' registered individual with start/end/supportcode and support description}
-#' \item{branche}{A long version data.table providing a record per month for
+#' \item{branche}{  A long version data.table providing a record per month for
 #' type of profession with start/end/work-code and description}
 #' }
 #' The description provided must be merged from a text file with matching code
 #' and description.  These text-files can be found in statistics Denmark in the
 #' area holding standard Statistics Denmark formats: "//srvfsenas1/formater/
-#' SAS formater i Danmarks Statistik/txt_filer". Multiple formats are available
-#' for more or less crude grouping.
-#' @usage
-#' importDREAM(dreamData,explData=NULL,type="support",pnr="PNR")
+#' SAS formater i Danmarks Statistik/txt_file-r". Multiple formats are available
+#' for more or less crude grouping.  These files are SAS format files, but can
+#' easily be converted by removing start and end and other slight modifications.
+#' @usage  
+#' importDREAM(dreamData,explData=NULL,type="support",pnr="pnr")
 #' @author Christian Torp-Pedersen
 #' @param dreamData Name of dataset holding DREAM data. Typically product of
 #' the importSAS function.  In most cases it will be useful to add a set.hook
@@ -64,60 +66,72 @@
 #' temp2 <- importDREAM(microDREAM,support,type="support",pnr="PNR")
 #' temp2[]         
 #' @export
-importDREAM <- function(dreamData,explData=NULL,type="support",pnr="PNR")
+importDREAM <- function (dreamData, explData = NULL, type = "support", 
+                         pnr = "pnr") 
 {
-  .SD<-melt<-patterns<-start<-num_b<-branche<-lastdate<-.N<-end<-char_week<-week<-year<-support<-na.omit<-NULL
+  .SD <- melt <- patterns <- start <- num_b <- branche <- lastdate <- .N <- end <- char_week <- week <- year <- support <- na.omit <- NULL
   setDT(dreamData)
-  if (!is.null(explData)){
+  if (!is.null(explData)) {
     setDT(explData)
-    if (!type %in% names(explData)) stop('One column in explData must be type in order to correctly merge')
-  } 
-  if (!type %in% c("support","branche")) stop('type must be "support" or "branche"')
-  if (type=="branche"){
-    cols<-c(pnr,grep("branch",names(dreamData),value=TRUE))
-    DREAM <- dreamData[,.SD,.SDcols=cols]
-    changeCols <- cols[-1] # other than pnr
-    DREAM <- DREAM[,(changeCols):=lapply(.SD,as.character),.SDcols=changeCols] # made all branche as character
-    DREAM <- melt(DREAM,measure=patterns("branch"),variable.name="date",value.name="branche") #transpose
-    #Define start of periods
-    DREAM[,start:=as.Date(paste0(gsub("branche_","",as.character(date)),"_01"),format="%Y_%m_%d")] 
-    setkeyv(DREAM,c("PNR","start")) #sort
-    DREAM[,num_b:=as.numeric(branche)] #Numeric version of branche - requirement for diff
-    DREAM[is.na(num_b),num_b:=-1] #diff must have integers, not NA
-    DREAM[,lastdate:=seq(start[.N],by="1 months",length=2)[2],by="PNR"] # Last date each PNR + 1 month
-    DREAM[,diff:=c(999,diff(num_b,lag=1))] # difference between columns with NA for first
-    DREAM <- DREAM[diff!=0] # remove repetitions - leaves 1 record for each period of branche
-    DREAM[,end:=shift(start,type="lead")] # provides end dates
-    DREAM[is.na(end),end:=lastdate] #provides end date for last record of each PNR
-    DREAM[,c("date","num_b","diff","lastdate"):=NULL] # remove temporary columns
+    if (!type %in% names(explData)) 
+      stop("One column in explData must be type in order to correctly merge")
   }
-  else{
-    cols<-c(pnr,grep("y_",names(dreamData),value=TRUE))
-    DREAM <- dreamData[,.SD,.SDcols=cols]
-    changeCols <- cols[-1] # other than pnr
-    DREAM <- DREAM[,(changeCols):=lapply(.SD,as.character),.SDcols=changeCols] # made all support as character
-    DREAM <- melt(DREAM,measure=patterns("y_"),variable.name="date",value.name="support") #transpose
-    #Define start of periods
-    DREAM[,':='(year=as.integer(substr(date,3,4)),week=as.integer(substr(date,5,6)))]
-    DREAM[,char_week:=ifelse(week<10,as.character(paste0('0',week)),as.character(week))]
-    DREAM[,year:=ifelse(year<90,year+2000L,year+1900L)]
-    DREAM[,start:=ISOweek::ISOweek2date(paste0(year,"-W",char_week,"-1"))] 
-    setkeyv(DREAM,c("PNR","start")) #sort
-    DREAM[,num_b:=as.numeric(support)] #Numeric version of support - requirement for diff
-    DREAM[is.na(num_b),num_b:=-1] #diff must have integers, not NA
-    DREAM[,lastdate:=start[.N]+7,by="PNR"] # Last date each PNR + 1 week
-    DREAM[,diff:=c(999,diff(num_b,lag=1))] # difference between columns with NA for first
-    DREAM <- DREAM[diff!=0] # remove repetitions - leaves 1 record for each period of support
-    DREAM[,end:=shift(start,type="lead")] # provides end dates
-    DREAM[is.na(end),end:=lastdate] #provides end date for last record of each PNR
-    DREAM[,c("date","num_b","diff","lastdate","year","week","char_week"):=NULL] # remove temporary columns
+  if (!type %in% c("support", "branche")) 
+    stop("type must be \"support\" or \"branche\"")
+  if (type == "branche") {
+    cols <- c(pnr, grep("branch", names(dreamData), 
+                        value = TRUE))
+    DREAM <- dreamData[, .SD, .SDcols = cols]
+    changeCols <- cols[-1]
+    DREAM <- DREAM[, `:=`((changeCols), lapply(.SD, 
+                                               as.character)), .SDcols = changeCols]
+    DREAM <- melt(DREAM, measure = patterns("branch"), 
+                  variable.name = "date", value.name = "branche")
+    DREAM[, `:=`(start, as.Date(paste0(gsub("branche_", 
+                                            "", as.character(date)), "_01"), format = "%Y_%m_%d"))]
+    setkeyv(DREAM, c(pnr, "start"))
+    DREAM[, `:=`(num_b, as.numeric(branche))]
+    DREAM[is.na(num_b), `:=`(num_b, -1)]
+    DREAM[, `:=`(lastdate, seq(start[.N], by = "1 months", 
+                               length = 2)[2]), by = pnr]
+    DREAM[, `:=`(diff, c(999, diff(num_b, lag = 1)))]
+    DREAM <- DREAM[diff != 0]
+    DREAM[, `:=`(end, shift(start, type = "lead"))]
+    DREAM[is.na(end), `:=`(end, lastdate)]
+    DREAM[, `:=`(c("date", "num_b", "diff", 
+                   "lastdate"), NULL)]
   }
-  if (!is.null(explData)){
-    #merge
-    DREAM <- merge(DREAM,explData,by=type,all.x=TRUE)
-    setkeyv(DREAM,c(pnr,"start"))
+  else {
+    cols <- c(pnr, grep("y_", names(dreamData), value = TRUE))
+    DREAM <- dreamData[, .SD, .SDcols = cols]
+    changeCols <- cols[-1]
+    DREAM <- DREAM[, `:=`((changeCols), lapply(.SD, 
+                                               as.character)), .SDcols = changeCols]
+    DREAM <- melt(DREAM, measure = patterns("y_"), 
+                  variable.name = "date", value.name = "support")
+    DREAM[, `:=`(year = as.integer(substr(date, 3, 
+                                          4)), week = as.integer(substr(date, 5, 6)))]
+    DREAM[, `:=`(char_week, ifelse(week < 10, as.character(paste0("0", 
+                                                                  week)), as.character(week)))]
+    DREAM[, `:=`(year, ifelse(year < 90, year + 2000L, 
+                              year + 1900L))]
+    DREAM[, `:=`(start, ISOweek::ISOweek2date(paste0(year, 
+                                                     "-W", char_week, "-1")))]
+    setkeyv(DREAM, c(pnr, "start"))
+    DREAM[, `:=`(num_b, as.numeric(support))]
+    DREAM[is.na(num_b), `:=`(num_b, -1)]
+    DREAM[, `:=`(lastdate, start[.N] + 7), by = pnr]
+    DREAM[, `:=`(diff, c(999, diff(num_b, lag = 1)))]
+    DREAM <- DREAM[diff != 0]
+    DREAM[, `:=`(end, shift(start, type = "lead"))]
+    DREAM[is.na(end), `:=`(end, lastdate)]
+    DREAM[, `:=`(c("date", "num_b", "diff", 
+                   "lastdate", "year", "week", "char_week"), 
+                 NULL)]
+  }
+  if (!is.null(explData)) {
+    DREAM <- merge(DREAM, explData, by = type, all.x = TRUE)
+    setkeyv(DREAM, c(pnr, "start"))
   }
   DREAM
 }
-
-
