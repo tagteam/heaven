63bc7a4db4efb12283936e253f5da22195fa5f93
Christian Torp-Pedersen <christian.torp-pedersen@regionh.dk>
2026-02-16 13:48:25 +0100

    Changed heaven-package.R to remove garbage in the file

--- R/heaven-package.R
+++ R/heaven-package.R
@@ -1,15 +1,4 @@
-#' heaven: Tools for Statitics Denmark
+#' @title heaven
+#' @description Package-level documentation for the **heaven** package.
 #' @keywords internal
 "_PACKAGE"
-#' @name heaven
-#' @useDynLib heaven
-#' @importFrom Rcpp sourceCpp evalCpp
-#' @importFrom data.table fread as.data.table setkeyv is.data.table data.table ":=" setnames setorder setcolorder setkey rbindlist setkeyv set copy shift setDT
-#' @importFrom foreach "%dopar%" foreach
-#' @importFrom grDevices colorRampPalette topo.colors
-#' @importFrom graphics points title
-#' @importFrom scales date_format
-#' @importFrom ggplot2 ggplot aes facet_wrap theme_bw guides geom_point geom_line guide_legend scale_fill_manual theme xlab element_rect scale_x_date unit scale_color_manual
-#' @importFrom stats D predict qnorm rbinom rnorm runif setNames
-#' @importFrom utils packageVersion setTxtProgressBar tail txtProgressBar
-NULL
