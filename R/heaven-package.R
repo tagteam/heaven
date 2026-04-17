7c6b8620dc108b24978e0364a496d90b81901524
Thomas Alexander Gerds <tag@biostat.ku.dk>
2026-04-17 13:40:10 +0200

    import data.table

--- R/heaven-package.R
+++ R/heaven-package.R
@@ -2,5 +2,5 @@
 #' @description Package-level documentation for the **heaven** package.
 #' @useDynLib heaven, .registration=TRUE
 #' @keywords internal
+#' @importFrom data.table data.table set dcast setkeyv as.data.table copy data.table is.data.table melt rbindlist setnames setorder setorderv setcolorder setkey ":=" ".N" ".SD" 
 "_PACKAGE"
- 
